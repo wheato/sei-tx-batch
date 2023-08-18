6713d6408a67c87fe472a472034131fcabd0572f
wheato <wheato2010@gmail.com>
2023-08-18 18:50:55 +0800

    feat: query

--- pages/api/sei.ts
+++ pages/api/sei.ts
@@ -1,4 +1,5 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import axios from 'axios';
 import type { NextApiRequest, NextApiResponse } from 'next'
 
 type Data = any[];
@@ -7,52 +8,8 @@ const REMOTE_URL =
   "https://pacific-1.albatross.sei-internal.com/connected?seiAddress=";
 
 async function getWalletData (addr: string) {
-  // const res = await fetch(`${REMOTE_URL}${addr}`, {
-  //   method: "GET",
-  //   headers: {
-  //     accept:
-  //       "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
-  //     "accept-language": "zh-HK,zh-TW;q=0.9,zh-CN;q=0.8,zh;q=0.7,en;q=0.6",
-  //     "cache-control": "max-age=0",
-  //     "if-none-match": 'W/"101-UOYOIRlYEwma6HnHBkH47AKHUco"',
-  //     "sec-ch-ua":
-  //       '"Not/A)Brand";v="99", "Google Chrome";v="115", "Chromium";v="115"',
-  //     "sec-ch-ua-mobile": "?0",
-  //     "sec-ch-ua-platform": '"macOS"',
-  //     "sec-fetch-dest": "document",
-  //     "sec-fetch-mode": "navigate",
-  //     "sec-fetch-site": "none",
-  //     "sec-fetch-user": "?1",
-  //     "upgrade-insecure-requests": "1",
-  //   },
-  //   referrerPolicy: "strict-origin-when-cross-origin",
-  //   body: null,
-  // });
-  const res = await fetch(
-    "https://pacific-1.albatross.sei-internal.com/connected?seiAddress=sei1vg54rkwq8tdnf5uf80kxchlvlr6c4lnh2p6d72",
-    {
-      headers: {
-        accept:
-          "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
-        "accept-language": "zh-HK,zh-TW;q=0.9,zh-CN;q=0.8,zh;q=0.7,en;q=0.6",
-        "cache-control": "max-age=0",
-        "if-none-match": 'W/"101-UOYOIRlYEwma6HnHBkH47AKHUco"',
-        "sec-ch-ua":
-          '"Not/A)Brand";v="99", "Google Chrome";v="115", "Chromium";v="115"',
-        "sec-ch-ua-mobile": "?0",
-        "sec-ch-ua-platform": '"macOS"',
-        "sec-fetch-dest": "document",
-        "sec-fetch-mode": "navigate",
-        "sec-fetch-site": "none",
-        "sec-fetch-user": "?1",
-        "upgrade-insecure-requests": "1",
-      },
-      referrerPolicy: "strict-origin-when-cross-origin",
-      body: null,
-      method: "GET",
-    }
-  );
-  return res.json();
+  const res = await axios.get(`${REMOTE_URL}${addr}`);
+  return res.data;
 }
 
 export default async function handler(
@@ -62,9 +19,23 @@ export default async function handler(
   const addrs = req.body.split('\n').map((addr: string) => addr.trim());
   const results = [];
   for (let i = 0; i < addrs.length; i++) {
-    console.log(addrs[i])
-    const data = await getWalletData(addrs[i]);
-    results.push(data);
+    try {
+      const data = await getWalletData(addrs[i]);
+      if (data.status !== 'success') {
+        results.push({ address: addrs[i], status: -1, usdValue: 0});
+        continue;
+      }
+      // 处理数据
+      if (!data.data.tx) {
+        results.push({ address: addrs[i], status: 0, usdValue: 0 });
+        continue;
+      }
+      const tx = data.data.tx;
+      const { usdValue, timestamp } = tx;
+      results.push({ address: addrs[i], status: 1, usdValue, timestamp });
+    } catch (err) {
+      results.push({ address: addrs[i], status: -1, usdValue: 0 });
+    }
   }
   res.status(200).json(results);
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,31 +1,64 @@
-import Image from 'next/image'
 import { Inter } from 'next/font/google'
 import { useState } from 'react';
+import dayjs from 'dayjs';
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   const [results, setResults] = useState<any[]>([]);
   const [inputs, setInputs] = useState('');
+  const [loading, setLoading] = useState(false);
 
   const onQuery = async () => {
+    if (!inputs.trim() || loading) {
+      return;
+    }
+    setLoading(true);
     try {
       const res = await fetch('/api/sei', {
         method: 'POST',
         body: inputs,
       });
+      
       const data = await res.json();
-      console.log(data);
+      setResults(data);
     } catch (err) {
+      console.log(err);
       alert('网络错误！')
+    } finally {
+      setLoading(false);
     }
   }
 
   return (
-    <main className={`flex min-h-screen flex-col px-24 py-12 ${inter.className}`}>
+    <main
+      className={`flex min-h-screen flex-col px-24 py-12 ${inter.className}`}
+    >
       <h2 className="mb-4 text-3xl font-extrabold text-center leading-none tracking-tight text-gray-900 md:text-4xl lg:text-5xl">
         Sei 跨链到账批量查询
       </h2>
+      <p className="flex justify-end text-left inline">
+        <a
+          href="https://twitter.com/cc_loky"
+          target="_blank"
+          className="flex items-center text-blue-600 text-sm dark:text-blue-500 hover:underline"
+        >
+          <svg
+            className="w-4 h-4 mr-2"
+            aria-hidden="true"
+            xmlns="http://www.w3.org/2000/svg"
+            fill="currentColor"
+            viewBox="0 0 20 17"
+          >
+            <path
+              fill-rule="evenodd"
+              d="M20 1.892a8.178 8.178 0 0 1-2.355.635 4.074 4.074 0 0 0 1.8-2.235 8.344 8.344 0 0 1-2.605.98A4.13 4.13 0 0 0 13.85 0a4.068 4.068 0 0 0-4.1 4.038 4 4 0 0 0 .105.919A11.705 11.705 0 0 1 1.4.734a4.006 4.006 0 0 0 1.268 5.392 4.165 4.165 0 0 1-1.859-.5v.05A4.057 4.057 0 0 0 4.1 9.635a4.19 4.19 0 0 1-1.856.07 4.108 4.108 0 0 0 3.831 2.807A8.36 8.36 0 0 1 0 14.184 11.732 11.732 0 0 0 6.291 16 11.502 11.502 0 0 0 17.964 4.5c0-.177 0-.35-.012-.523A8.143 8.143 0 0 0 20 1.892Z"
+              clip-rule="evenodd"
+            />
+          </svg>
+          欢迎关注我的 Twitter，DDDD
+        </a>
+      </p>
       <div className="block mb-2 text-sm font-medium text-gray-300 dark:text-gray-800 text-left">
         填写要查询的 Sei 地址
       </div>
@@ -37,13 +70,40 @@ export default function Home() {
         onChange={(e) => setInputs(e.target.value)}
       ></textarea>
       <div className="mb-8">
-        <button
-          onClick={onQuery}
-          type="button"
-          className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none"
-        >
-          查询
-        </button>
+        {loading ? (
+          <button
+            disabled
+            type="button"
+            className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 inline-flex items-center"
+          >
+            <svg
+              aria-hidden="true"
+              role="status"
+              className="inline w-4 h-4 mr-3 text-white animate-spin"
+              viewBox="0 0 100 101"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
+                fill="#E5E7EB"
+              />
+              <path
+                d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
+                fill="currentColor"
+              />
+            </svg>
+            Loading...
+          </button>
+        ) : (
+          <button
+            onClick={onQuery}
+            type="button"
+            className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none"
+          >
+            查询
+          </button>
+        )}
       </div>
       <h3 className="mb-4 text-center text-2xl font-extrabold leading-none tracking-tight text-gray-900 md:text-3xl lg:text-4xl">
         查询结果
@@ -61,19 +121,43 @@ export default function Home() {
               <th scope="col" className="px-6 py-3">
                 到账金额
               </th>
+              <th scope="col" className="px-6 py-3">
+                到账时间
+              </th>
             </tr>
           </thead>
           <tbody>
             {results.map((result) => (
-              <tr className="bg-white border-b" key={result.addr}>
+              <tr className="bg-white border-b" key={result.address}>
                 <th
                   scope="row"
                   className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap"
                 >
-                  {result.addr}
+                  {result.address}
                 </th>
-                <td className="px-6 py-4">{result.status}</td>
-                <td className="px-6 py-4">{result.usdValue}</td>
+                <td
+                  className={`px-6 py-4 ${
+                    result.status > 0
+                      ? "text-green-600"
+                      : result.status === -1
+                      ? "text-red-600"
+                      : "text-orange-500"
+                  }`}
+                >
+                  {result.status > 0
+                    ? "成功"
+                    : result.status === -1
+                    ? "错误"
+                    : "未到账"}
+                </td>
+                <td className="px-6 py-4">
+                  {result.usdValue ? `$${result.usdValue}` : "N/A"}
+                </td>
+                <td className="px-6 py-4">
+                  {result.timestamp
+                    ? `${dayjs(result.timestamp).format("YYYY-MM-DD HH:mm:ss")}`
+                    : "N/A"}
+                </td>
               </tr>
             ))}
           </tbody>
