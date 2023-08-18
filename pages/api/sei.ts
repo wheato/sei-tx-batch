bbc2608ef2829896e0c919e60b5e17430709b86d
wheato <wheato2010@gmail.com>
2023-08-18 17:53:27 +0800

    init

---
+++ pages/api/sei.ts
@@ -0,0 +1,70 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import type { NextApiRequest, NextApiResponse } from 'next'
+
+type Data = {
+  name: string
+}
+const REMOTE_URL =
+  "https://pacific-1.albatross.sei-internal.com/connected?seiAddress=";
+
+async function getWalletData (addr: string) {
+  // const res = await fetch(`${REMOTE_URL}${addr}`, {
+  //   method: "GET",
+  //   headers: {
+  //     accept:
+  //       "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
+  //     "accept-language": "zh-HK,zh-TW;q=0.9,zh-CN;q=0.8,zh;q=0.7,en;q=0.6",
+  //     "cache-control": "max-age=0",
+  //     "if-none-match": 'W/"101-UOYOIRlYEwma6HnHBkH47AKHUco"',
+  //     "sec-ch-ua":
+  //       '"Not/A)Brand";v="99", "Google Chrome";v="115", "Chromium";v="115"',
+  //     "sec-ch-ua-mobile": "?0",
+  //     "sec-ch-ua-platform": '"macOS"',
+  //     "sec-fetch-dest": "document",
+  //     "sec-fetch-mode": "navigate",
+  //     "sec-fetch-site": "none",
+  //     "sec-fetch-user": "?1",
+  //     "upgrade-insecure-requests": "1",
+  //   },
+  //   referrerPolicy: "strict-origin-when-cross-origin",
+  //   body: null,
+  // });
+  const res = await fetch(
+    "https://pacific-1.albatross.sei-internal.com/connected?seiAddress=sei1vg54rkwq8tdnf5uf80kxchlvlr6c4lnh2p6d72",
+    {
+      headers: {
+        accept:
+          "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
+        "accept-language": "zh-HK,zh-TW;q=0.9,zh-CN;q=0.8,zh;q=0.7,en;q=0.6",
+        "cache-control": "max-age=0",
+        "if-none-match": 'W/"101-UOYOIRlYEwma6HnHBkH47AKHUco"',
+        "sec-ch-ua":
+          '"Not/A)Brand";v="99", "Google Chrome";v="115", "Chromium";v="115"',
+        "sec-ch-ua-mobile": "?0",
+        "sec-ch-ua-platform": '"macOS"',
+        "sec-fetch-dest": "document",
+        "sec-fetch-mode": "navigate",
+        "sec-fetch-site": "none",
+        "sec-fetch-user": "?1",
+        "upgrade-insecure-requests": "1",
+      },
+      referrerPolicy: "strict-origin-when-cross-origin",
+      body: null,
+      method: "GET",
+    }
+  );
+  return res.json();
+}
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<Data>
+) {
+  const addrs = req.body.split('\n').map((addr: string) => addr.trim());
+  for (let i = 0; i < addrs.length; i++) {
+    console.log(addrs[i])
+    const data = await getWalletData(addrs[i]);
+    console.log(data);
+  }
+  res.status(200).json({ name: 'John Doe' })
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,118 +1,84 @@
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
+import { useState } from 'react';
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
+  const [results, setResults] = useState<any[]>([]);
+  const [inputs, setInputs] = useState('');
+
+  const onQuery = async () => {
+    try {
+      const res = await fetch('/api/sei', {
+        method: 'POST',
+        body: inputs,
+      });
+      const data = await res.json();
+      console.log(data);
+    } catch (err) {
+      alert('网络错误！')
+    }
+  }
+
   return (
-    <main
-      className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
-    >
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">pages/index.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
+    <main className={`flex min-h-screen flex-col px-24 py-12 ${inter.className}`}>
+      <h2 className="mb-4 text-3xl font-extrabold text-center leading-none tracking-tight text-gray-900 md:text-4xl lg:text-5xl">
+        Sei 跨链到账批量查询
+      </h2>
+      <div className="block mb-2 text-sm font-medium text-gray-300 dark:text-gray-800 text-left">
+        填写要查询的 Sei 地址
       </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700/10 after:dark:from-sky-900 after:dark:via-[#0141ff]/40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
+      <textarea
+        id="message"
+        rows={15}
+        className="block p-2.5 mb-4 w-full text-sm text-gray-900 bg-gray-100 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500"
+        placeholder="填写 Sei 地址，一行一个地址"
+        onChange={(e) => setInputs(e.target.value)}
+      ></textarea>
+      <div className="mb-8">
+        <button
+          onClick={onQuery}
+          type="button"
+          className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none"
+        >
+          查询
+        </button>
       </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Discover and deploy boilerplate example Next.js&nbsp;projects.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+      <h3 className="mb-4 text-center text-2xl font-extrabold leading-none tracking-tight text-gray-900 md:text-3xl lg:text-4xl">
+        查询结果
+      </h3>
+      <div className="w-full relative overflow-x-auto">
+        <table className="w-full text-sm text-left text-gray-500">
+          <thead className="text-xs text-gray-700 uppercase bg-gray-50">
+            <tr>
+              <th scope="col" className="px-6 py-3">
+                地址
+              </th>
+              <th scope="col" className="px-6 py-3">
+                是否到账
+              </th>
+              <th scope="col" className="px-6 py-3">
+                到账金额
+              </th>
+            </tr>
+          </thead>
+          <tbody>
+            {results.map((result) => (
+              <tr className="bg-white border-b" key={result.addr}>
+                <th
+                  scope="row"
+                  className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap"
+                >
+                  {result.addr}
+                </th>
+                <td className="px-6 py-4">{result.status}</td>
+                <td className="px-6 py-4">{result.usdValue}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
       </div>
     </main>
-  )
+  );
 }
